de2967f31e871705d138c0df942cca9c8f0afbd4
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-05 10:46:08 -0500

    Product of 2  numbers

---
+++ Product.py
@@ -0,0 +1,6 @@
+# Write code to find the product of 2 numbers
+
+Num1 = input("Enter first number: ")
+Num2 = input("Enter second number: ")
+Product = float(Num1) * float(Num2)
+print (f"The product of {Num1} and {Num2} is: {Product}")
