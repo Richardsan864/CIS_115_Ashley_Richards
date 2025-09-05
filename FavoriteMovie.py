e0d1ce4b5fc6261010cacd7edcce2c458c146909
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-05 11:34:59 -0500

    Favorite Movie

---
+++ FavoriteMovie.py
@@ -0,0 +1,6 @@
+# Write code for favorite movie and characters.
+
+Movie = "Clue"
+Characters = "Miss Scarlet", "Colonel Mustard", "Mr. Green", "Mrs. Peacock", "Wadsworth", "Mrs. White", "Professor Plum"
+print ("My favorite movie is: ", Movie)
+print ("The main characters are: ", Characters)
