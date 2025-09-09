3b9666721aca143f6a30d94841626cb1031e6c2c
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-09 08:38:43 -0500

    MathmaticalOperations1

---
+++ Week4Lab/MathmaticalOperation.py
@@ -0,0 +1,8 @@
+#Ask user for imput
+Num1 = float(input("Enter first number: "))
+Num2 = float(input("Enter second number: "))
+Num3 = float(input("Enter third number: "))
+Add = (Num1) + (Num2)
+divide = (Add) / float(Num3)
+print("The answer is: ", divide)
+
