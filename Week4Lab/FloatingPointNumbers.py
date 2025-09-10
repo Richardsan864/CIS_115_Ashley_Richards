ed734894e2c215aee6713def5b1bd4a8c22d5b38
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-10 09:42:52 -0500

    FloatingPointNumber

---
+++ Week4Lab/FloatingPointNumbers.py
@@ -0,0 +1,7 @@
+# Use integer division to divide two floating point numbers
+
+num1 = input("Enter first floating point number: ")
+num2 = input("Enter second floating point number: ")
+
+result = float(num1) / float(num2)
+print("The answer is ", int(result))
