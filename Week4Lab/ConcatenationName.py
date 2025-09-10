500aea1e78e97b847f5e491c2943e2667278fa5e
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-10 09:54:22 -0500

    Concatenate first and last name

---
+++ Week4Lab/ConcatenationName.py
@@ -0,0 +1,5 @@
+#Write code to concatenate first name and last name.
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("Hello,", full_name)
