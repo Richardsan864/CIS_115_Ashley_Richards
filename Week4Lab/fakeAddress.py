634176d8e4c52274412d28b9aa05bf201fd19c58
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-10 10:17:11 -0500

    Seperate lines

---
+++ Week4Lab/fakeAddress.py
@@ -0,0 +1,8 @@
+# Write code to list an address on seperate lines
+
+street = input("Enter your street address: ")
+city = input("Enter your city: ")
+state = input("Enter your state: ")
+zip = input("Enter your zip code: ")
+
+print(f"{street}\n{city}\n{state}\n{zip}")
