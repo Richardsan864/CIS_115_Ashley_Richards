1d24c70ff6fbe71f843d484306487f5d724ab7e1
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-13 21:42:17 -0500

    new credit card information

---
+++ Week4Lab/CreditCard.py
@@ -0,0 +1,9 @@
+# Write a code using input function to obtain credit card information.
+
+credit_number = input("Enter your credit card number: ")
+exp_date = input("Enter your cards expiration date: ")
+cvv = input("Enter your CVV: ")
+
+#Print credit card information using print functions seperation argument
+
+print(f"Credit Card Number: {credit_number}\nExperation date: {exp_date}\nCVV: {cvv}")
