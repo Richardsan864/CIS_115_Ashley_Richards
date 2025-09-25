346c5b735ce2b3026464d5bd9c46399d3a302e8b
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-25 19:12:25 -0400

    edit calculateTax.py

--- Week4Lab/Week5Lab/calculateTax.py
+++ Week4Lab/Week5Lab/calculateTax.py
@@ -3,4 +3,4 @@
 num1 = float(75.34) # price of item
 sales_tax = float(0.0725)
 total = (num1 * sales_tax) + num1
-print("The total price is $ : {total:.2f}".format(total =total))
+print("The total price is $ : ", total)
