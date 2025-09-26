c7635d89f399311de106e031f962a731b93a42bf
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-26 12:31:39 -0400

    Finished Product

--- hypoteneuse.py
+++ hypoteneuse.py
@@ -1,10 +1,13 @@
 import math
 
 # Get user input for 2 sides of the triangle.
+# Assign the numbers to a variable
 
-side_a = float(input("Enter the length of side A: "))
-side_b = float(input("Enter the length of side B: "))
+side_x = float(input("Enter the length of side A: "))
+side_y = float(input("Enter the length of side B: "))
 
 # Enter the formula for the Pathagorean Theorem and assign it to a variable.
 
-theorum = 
+theorum = float(math.sqrt(side_x**2 + side_y**2))
+
+print("The hypoteneuse is: ", theorum)
