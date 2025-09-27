08605446508ec952704201ecae60d93286e4fe83
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-27 17:20:07 -0400

    added joules

--- massAccelerationEnergy.py
+++ massAccelerationEnergy.py
@@ -1,21 +1,20 @@
-# Calculate first given problem.
+# Write a function where mass = 3kg and c = 2.99 x 10**8.
 
-mass = float(3)
-acceleration = float(2.99 * 10**8)
-energy = mass * acceleration
-print("The energy is", energy, "Joules")
+def energy_problem():
+    mass = float(3)
+    acceleration = float(2.99 * 10**8)
+    energy = mass * acceleration
+    print("The energy produced using 3kg of mass is", float(energy), "Joules")
 
+energy_problem()
 
-#Get user input for amount of mass (in kg).
+# Write a def function to set up the calculation problem.
+# Make sure the numbers are a floating point value.
+# Ask for user input for mass in kg.
 
-calculate_energy = float(input("Enter the mass: "))
-
-# Assign a variable to accelaration.
-
-acceleration = float(2.99 * 10**8)
-
-# Calculate the energy in Joules.
-
-energy = calculate_energy * acceleration
-
-print("The energy is", energy, "Joules")
+def calculate_energy():
+    mass = float(input("Enter the mass in kg: "))
+    c = float(2.99 * 10**8)
+    acceleration = mass * c
+    print("The energy produced is: ", acceleration, "Joules")
+calculate_energy()
