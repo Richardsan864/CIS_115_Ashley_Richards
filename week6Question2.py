99c757d1a6a35bf93288f91dd0fcc457bca200e9
Ashley Richards <richardsan864@mail.sandhills.edu>
2025-09-27 22:11:31 -0400

    add messages

--- week6Question2.py
+++ week6Question2.py
@@ -1,9 +1,12 @@
-# Define your 2 functions names.
+# Make a user defined function named print_message1.
+# Under this function print the first statement.
 
 def print_message1():
     print("I was called first")
     print_message2()
 
+# Define antoher function named print_message2.
+# In this function print statement 2.
 def print_message2():
     print("I was called from print_message1")
 
